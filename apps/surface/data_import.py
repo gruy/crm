a02e96ae3c693ad03cd5d60adea2f793c82ba2c6
Кирилл Попов <k.popov@atol.ru>
2022-04-21 14:44:34 +0300

    #21 Исправление загрузки новых адресов из Excel

--- apps/surface/data_import.py
+++ apps/surface/data_import.py
@@ -13,16 +13,16 @@ def address_list_import(request):
     if request.method == 'POST' and 'file' in request.FILES:
         filename = request.FILES['file'].name
         extension = filename.split(".")[1]
-        sheet = pyexcel.load_from_memory(extension, request.FILES['file'].read())
-        data = pyexcel.to_dict(sheet)
-        for row in data:
+        content = request.FILES['file'].read()
+        sheet = pyexcel.get_sheet(file_type=extension, file_content=content, start_row=1)
+        for row in sheet:
             if row != 'Series_1':
-                city = data[row][0]
-                area = data[row][1]
-                street = data[row][2]
+                city = row[0]
+                area = row[1]
+                street = row[2]
                 house_number = ''
                 point_flag = False
-                for i in unicode(data[row][3]):
+                for i in row[3]:
                     if i == '.':
                         point_flag = True
                     if point_flag:
@@ -42,7 +42,7 @@ def address_list_import(request):
                 #         except:
                 #             pass
                 try:
-                    raw_porch_list = str(data[row][4]).replace('.', ',').strip().split(',')
+                    raw_porch_list = str(row[4]).replace('.', ',').strip().split(',')
                 except UnicodeEncodeError:
                     continue
                 # import pdb; pdb.set_trace()
@@ -51,11 +51,11 @@ def address_list_import(request):
                     if i.strip().isdigit() and i.strip() != '0' or i.strip() != '':
                         porch_list.append(int(i.strip()))
                 try:
-                    if str(data[row][5]).isdigit():
-                        floors = data[row][5]
+                    if str(row[5]).isdigit():
+                        floors = row[5]
                     else:
                         floors = None
-                        floor_data = str(data[row][5])
+                        floor_data = str(row[5])
                         if floor_data.__contains__('.'):
                             floor_data = floor_data.split('.')
                             if len(floor_data) == 2 and floor_data[1] == '0' and floor_data[0].isdigit():
@@ -63,11 +63,11 @@ def address_list_import(request):
                 except:
                     floors = None
                 try:
-                    if str(data[row][6]).isdigit():
-                        apart_count = data[row][6]
+                    if str(row[6]).isdigit():
+                        apart_count = row[6]
                     else:
                         apart_count = None
-                        apart_count_data = str(data[row][6])
+                        apart_count_data = str(row[6])
                         if apart_count_data.__contains__('.'):
                             apart_count_data = apart_count_data.split('.')
                             if len(apart_count_data) == 2 and apart_count_data[1] == '0' and apart_count_data[0].isdigit():
