d9dec6bbd9d4738fd38065a101ec29d264f60547
Cyrill Popov <gruy@feodosia.net>
2023-11-15 16:28:24 +0300

    #21 Обрезка лишних символов до и после слов при импорте адресов

--- apps/surface/data_import.py
+++ apps/surface/data_import.py
@@ -23,17 +23,17 @@ def address_list_import(request):
         sheet = pyexcel.get_sheet(file_type=extension, file_content=content, start_row=1)
         for row in sheet:
             if row != 'Series_1':
-                city = row[0]
-                area = row[1]
-                street = row[2]
-                house_number = row[3]
+                city = row[0].strip()
+                area = row[1].strip()
+                street = row[2].strip()
+                house_number = row[3].strip()
                 try:
-                    porches = int(row[4])
+                    porches = int(row[4].strip())
                 except ValueError:
                     porches = 0
-                company_name = row[5]
+                company_name = row[5].strip()
                 try:
-                    floors = int(row[6])
+                    floors = int(row[6].strip())
                 except ValueError:
                     floors = None
 
@@ -66,7 +66,9 @@ def address_list_import(request):
                         )
                     except Surface.DoesNotExist:
                         # создаём поверхность
-                        surface_instance = Surface(city=city_instance, street=street_instance, house_number=house_number)
+                        surface_instance = Surface(
+                            city=city_instance, street=street_instance, house_number=house_number
+                        )
 
                     try:
                         surface_instance.management = ManagementCompany.objects.get(
@@ -80,7 +82,7 @@ def address_list_import(request):
 
                     surface_instance.save()
 
-                    for i in range(porches):
+                    for i in range(1, porches + 1):
                         # пробегаемся по списку подъездов
                         try:
                             Porch.objects.get(surface=surface_instance, number=i)
